7eab6e64016dd4243d21c78ecc30260be5144922
YarosThk <yaroslavtashak@gmail.com>
2022-12-11 17:55:03 +0100

    Started to set up the production build

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors'); // to add colors in console messages
 const dotenv = require('dotenv').config(); // to have access to -env variables
-const { logErrors, errorHandler } = require('./middleware/errorHandler');
+const { errorHandler } = require('./middleware/errorHandler');
 const { connectDB } = require('./config/db');
 
 const port = process.env.PORT || 3000;
@@ -17,6 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/books', require('./routes/bookRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/comments', require('./routes/commentRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.statis(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => res.send('Please set to production environment'));
+}
 
 // app.use(logErrors);
 app.use(errorHandler);
